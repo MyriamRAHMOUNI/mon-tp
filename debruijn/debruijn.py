8f5a6a885fa47b4b40d26b85bc8ea0c4183829a5
myriamrahmouni <myriamrahmouni2016@gmail.com>
2019-10-22 14:57:32 +0200

    Identification des k-mer unique

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -1,3 +1,7 @@
+'''L’objectif de ce TP sera d’assembler le génome de l’entérovirus A71. Ce génome présente
+l’intérêt d’être très court: 7408 nucléotides, linéaire et non segmenté.'''
+
+
 import sys
 import argparse
 import networkx 
@@ -13,19 +17,36 @@ def get_arguments():
     args=parser.parse_args()
     return args
 
+###Création du graphe de de Bruijn
+##Identification des k-mer unique et les mettre dans un dictionnaire 
+
+def read_fastq(fichier_fastq):
+    with open(fichier_fastq, 'r') as f:
+        lines= iter(f.readlines())
+        for line in lines:
+           yield next(lines)
+           next(lines)
+           next(lines) 
+
+def cut_kmer(seq, taillekmer):
+    for i in range(len(seq)-taillekmer+1):
+        yield seq[i:taillekmer]
 
 
-
-
-
-
-pass
-
-
-
+def build_kmer_dict(fichier_fastq, taille_kmer):
+    dict_kmer={}
+    for x in read_fastq(fichier_fastq):
+        for kmer in cut_kmer(x, taille_kmer):
+            if kmer not in dict_kmer.keys():
+                dict_kmer[kmer] = 1
+            else:
+                dict_kmer[kmer] += 1
+    return dict_kmer
+ 
 
 if __name__ == "__main__":
-
+    print(sys.argv[2])
     arguments=get_arguments()
-    
+    a = build_kmer_dict('eva71_two_reads.fq', 3)
+    print(a)    
 
